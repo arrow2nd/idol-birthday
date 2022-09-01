45239890ca277de481c422af14df62774bceb38d
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-09-01 10:42:13 +0900

    chore: Tailwind CSS を導入

--- app/root.tsx
+++ app/root.tsx
@@ -8,11 +8,17 @@ import {
   ScrollRestoration
 } from '@remix-run/react'
 
+import styles from './styles/app.css'
+
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
   title: 'New Remix App',
   viewport: 'width=device-width,initial-scale=1'
 })
+
+export function links() {
+  return [{ rel: 'stylesheet', href: styles }]
+}
 
 export default function App() {
   return (
