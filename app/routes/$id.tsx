3a9662e74aaac37861e813a6c43453df924ce5d4
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-09-05 10:32:46 +0900

    feat(routes/$id): メタタグを追加

--- app/routes/$id.tsx
+++ app/routes/$id.tsx
@@ -1,4 +1,4 @@
-import { LoaderFunction } from '@remix-run/node'
+import { LoaderFunction, MetaFunction } from '@remix-run/node'
 import { useLoaderData } from '@remix-run/react'
 import invariant from 'tiny-invariant'
 
@@ -10,7 +10,7 @@ export const loader: LoaderFunction = ({ params }) => {
   // TODO: このIDから名前, 誕生日, パーソナルカラーを取得して返す
   return {
     id: params.id,
-    name: 'hoge',
+    name: '佐久間まゆ',
     birth: {
       month: 9,
       day: 7
@@ -20,6 +20,21 @@ export const loader: LoaderFunction = ({ params }) => {
       isWhitish: false
     }
   } as Idol
+}
+export const meta: MetaFunction<typeof loader> = ({ data }) => {
+  const title = `${data.name}さんのお誕生日まで...？ | idol-birthday`
+  const description = `${data.name}さんのお誕生日までの時間を秒単位でカウントダウンするサイトです`
+
+  return {
+    title,
+    description,
+    'og:site_name': title,
+    'og:title': title,
+    'og:description': description,
+    'twitter:card': 'summary',
+    'twitter:title': title,
+    'twitter:description': description
+  }
 }
 
 export default function IdolCountDownPage() {
