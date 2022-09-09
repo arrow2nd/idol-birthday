2e6c24050061a714c0a28215c1871971f81f4e6c
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-09-09 15:47:01 +0900

    feat(components/common/cards): カードがない場合の表示を変更

--- app/components/common/cards/index.tsx
+++ app/components/common/cards/index.tsx
@@ -22,9 +22,7 @@ export default function Cards({ title, idols }: Props) {
         {cards.length > 0 ? (
           cards
         ) : (
-          <p className="text-sub">
-            該当するアイドルがいないか、誕生日が公表されていません
-          </p>
+          <p className="text-sub">該当するアイドルがいません</p>
         )}
       </div>
     </>
