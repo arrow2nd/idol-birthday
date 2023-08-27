7062ba17796ce0eff394cd9614b2237ee00d0c95
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-08-27 20:24:40 +0900

    💬 サイト名・説明文を変更

--- app/data/site.ts
+++ app/data/site.ts
@@ -1,12 +1,12 @@
 const url = "https://idol-birthday.vercel.app"
 const name = "idol-birthday"
-const descTemplate = `%sのお誕生日までをカウントダウンするサイト`
+const descTemplate = `%sのお誕生日をカウントダウン`
 
 export const site = {
   url,
   defaultOgpImageUrl: url + "/ogp.png",
   name,
   title: `${name} | アイドルのお誕生日まで…？`,
-  desc: descTemplate.replace("%s", "アイマスアイドル"),
+  desc: descTemplate.replace("%s", "アイドル"),
   descTemplate
 }
