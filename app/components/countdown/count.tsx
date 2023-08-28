547a54f9136c8b4e29d45d1891bc2258b24327ff
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-08-28 23:47:18 +0900

    🚑 keyを追加

--- app/components/countdown/count.tsx
+++ app/components/countdown/count.tsx
@@ -16,13 +16,13 @@ export default function Count({ name, count }: Props): JSX.Element {
       <div>{`${name}さんのお誕生日まで`}</div>
       <div className="space-x-2">
         <span>残り</span>
-        <span
-          suppressHydrationWarning
-          className="text-5xl font-bold sm:text-6xl"
-        >
-          {numbers.map((n) => (
-            <span className="countdown">
-              <span style={{ "--value": n } as CSSProperties}></span>
+        <span className="text-5xl font-bold sm:text-6xl">
+          {numbers.map((n, i) => (
+            <span className="countdown" key={i}>
+              <span
+                suppressHydrationWarning
+                style={{ "--value": n } as CSSProperties}
+              ></span>
             </span>
           ))}
         </span>
