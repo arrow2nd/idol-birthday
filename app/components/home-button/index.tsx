336bf69315dfee581bc77677394ffee04cf5670c
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-09-21 20:27:57 +0900

    🚑 HOMEボタンが他の要素に隠れないように

--- app/components/home-button/index.tsx
+++ app/components/home-button/index.tsx
@@ -4,7 +4,7 @@ import { FiChevronLeft } from "react-icons/fi"
 export default function HomeButton(): JSX.Element {
   return (
     <Link
-      className="btn btn-wide fixed inset-x-0 bottom-10 m-auto rounded-full border-base-content"
+      className="btn btn-wide fixed inset-x-0 bottom-10 z-20 m-auto rounded-full border-base-content"
       to="/"
     >
       <FiChevronLeft />
