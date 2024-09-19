d302f1cab00f8f44d1856106d97d7ce4cd195163
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-09-19 21:14:23 +0900

    🚑 v1にマイグレ

--- app/routes/api.thumbnail.$name.tsx
+++ app/routes/api.thumbnail.$name.tsx
@@ -1,7 +1,6 @@
 import { LoaderFunctionArgs } from "@remix-run/node"
-import Jimp from "jimp"
+import { Jimp } from "jimp"
 import { idolImages } from "~/data/images"
-import { site } from "~/data/site"
 
 export async function loader({ params }: LoaderFunctionArgs) {
   const imageUrl = idolImages.get(params.name ?? "")
@@ -10,23 +9,18 @@ export async function loader({ params }: LoaderFunctionArgs) {
     return new Response(null, { status: 404 })
   }
 
-  // NOTE:
-  // Basic usage にオブジェクト(URLOptions)を渡す使い方があるものの、なぜか該当するオーバーロードがないので any にしています
   // https://github.com/jimp-dev/jimp/tree/main/packages/jimp#basic-usage
-  const image = await Jimp.read({
-    url: imageUrl,
-    headers: { "User-Agent": `getOgImageBot (${site.url})` }
-  } as any)
+  const image = await Jimp.read(imageUrl)
 
   // 小さめにリサイズ
   const resized = await image
-    .resize(448, Jimp.AUTO)
-    .getBufferAsync(Jimp.MIME_JPEG)
+    .resize({ w: 448 })
+    .getBuffer("image/jpeg", { quality: 80 })
 
   return new Response(resized, {
     status: 200,
     headers: {
-      "Content-Type": Jimp.MIME_JPEG,
+      "Content-Type": "image/jpeg",
       "Cache-Control": "public, max-age=31536000"
     }
   })
